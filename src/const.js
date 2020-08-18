715e90d0b9888e13ca45146b48bc4726579a28d4
Qiyuan <mqycn@126.com>
2020-08-18 19:54:11 +0800

    增加 常量定义

---
+++ src/const.js
@@ -0,0 +1,26 @@
+// 重新刷新文件
+const CommandRefresh = 'huile8.refresh';
+
+// 忽然又不会了
+const CommandWillMastering = 'huile8.willMastering';
+
+// 学会了
+const CommandDidMastered = 'huile8.didMastered';
+
+// 读单词
+const CommandRead = 'huile8.read';
+
+// 已经学会
+const Mastered = 1;
+
+// 还没学会
+const UnMastered = 0;
+
+module.exports = {
+  CommandRefresh,
+  CommandWillMastering,
+  CommandDidMastered,
+  CommandRead,
+  Mastered,
+  UnMastered
+};
