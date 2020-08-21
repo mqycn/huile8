2f60ef2f11975f128c0427ea25ef41d4b624b654
mqycn <mqycn@126.com>
2020-08-21 17:15:35 +0800

    增加 单词朗读面版，从 words.js 抽离

---
+++ src/read-panel.js
@@ -0,0 +1,46 @@
+// 创建播放窗口
+const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+
+const viewPath = path.join(__dirname, '..', 'view', 'list.html');
+const viewHtml = fs.readFileSync(viewPath, {
+    encoding: 'utf-8'
+});
+
+let _readPanel;
+
+module.exports = function () {
+    if (!_readPanel) {
+        const activeDocument = vscode.window.activeTextEditor.document;
+        const panel = _readPanel = vscode.window.createWebviewPanel(
+            'ReadPanel',
+            '会了吧：单词朗读',
+            vscode.ViewColumn.One,
+            {
+                enableScripts: true,
+                retainContextWhenHidden: true
+            }
+        );
+
+        // WebView内容
+        panel.webview.html = viewHtml;
+
+        // 读完关闭
+        panel.webview.onDidReceiveMessage((message) => {
+            vscode.window.showInformationMessage(`朗读: ${message}`);
+        });
+
+        // 关闭事件
+        panel.onDidDispose(() => {
+            _readPanel = null;
+        });
+
+        // 启动后激活之前的标签
+        setTimeout(() => {
+            vscode.window.showTextDocument(activeDocument);
+        }, 200);
+
+    }
+    return _readPanel.webview;
+};
