c4532fa77ad0b835abc85a41d8c4bb7d251aad14
Qiyuan <mqycn@126.com>
2020-09-08 00:22:52 +0800

    增加 状态开关显示

--- src/status-bar.js
+++ src/status-bar.js
@@ -1,22 +1,32 @@
 // 状态栏
 const vscode = require('vscode');
 const { CommandRefresh } = require('./const');
+const { autoRefresh } = require('./config');
+
+let defaultStatus;
+
+const getStatus = () => {
+  defaultStatus = autoRefresh.value ? '自动分析单词' : '点击分析单词';
+  return defaultStatus
+}
 
 const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+statusBar.text = getStatus();
 statusBar.show();
 statusBar.tooltip = '会了吧';
 statusBar.command = CommandRefresh;
 
-const statusDefault = '自动分析单词';
+autoRefresh.change(() => {
+  statusBar.text = getStatus();
+});
 
 let timerTask
-
 module.exports = {
   update(message) {
     statusBar.text = message;
-    timerTask && clearTimeout(timerTask);
+    !isNaN(timerTask) && clearTimeout(timerTask);
     timerTask = setTimeout(() => {
-      statusBar.text = statusDefault
-    }, 5000)
+      statusBar.text = defaultStatus;
+    }, 5000);
   }
-} 
+}
