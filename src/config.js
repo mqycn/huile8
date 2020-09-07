25404166d8d999e1a0744c98e21cfc9623feeb2d
Qiyuan <mqycn@126.com>
2020-09-08 00:21:00 +0800

    增加 配置项 调用代码

---
+++ src/config.js
@@ -0,0 +1,36 @@
+// 配置项
+const vscode = require('vscode');
+
+const configAll = [];
+
+class Config {
+  constructor(key) {
+    configAll.push(this);
+    this.key = key;
+    this.changeEvent = new vscode.EventEmitter();
+  }
+
+  get value() {
+    return vscode.workspace.getConfiguration().get(this.key);
+  }
+
+  set value(newVal) {
+    this.trigger();
+    vscode.workspace.getConfiguration().update(this.key, newVal, true);
+  }
+
+  change(func) {
+    this.changeEvent.event(func)
+  }
+
+  trigger() {
+    this.changeEvent.fire(null);
+  }
+}
+
+module.exports = {
+  autoRefresh: new Config('huile8.autoRefresh'),
+  configReload: vscode.workspace.onDidChangeConfiguration(() => {
+    configAll.forEach(item => item.trigger())
+  })
+}
